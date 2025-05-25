5068ed0ec4565fbc11f337b59d9149e272f1c0ef
Zehra Gul Yildiz <zehragul15@gmail.com>
2025-05-25 23:29:17 +0300

    Font added

--- lib/core/constants/app_text_styles.dart
+++ lib/core/constants/app_text_styles.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'app_colors.dart';
 
 class AppTextStyles {
-  static const TextStyle header = TextStyle(
-    fontSize: 28,
+  static TextStyle header = GoogleFonts.josefinSans(
+    fontSize: 35,
     fontWeight: FontWeight.bold,
-    color: AppColors.primaryText
+    color: AppColors.primaryText,
   );
 
-  static const TextStyle subtitle = TextStyle(
-    fontSize: 20,
+  static TextStyle subtitle = GoogleFonts.josefinSans(
+    fontSize: 25,
     fontWeight: FontWeight.w400,
     color: AppColors.secondaryText
   );
+
 }
