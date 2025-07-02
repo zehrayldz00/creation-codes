648a9918df4dad156f72e6b4c60799f42248eef5
Zehra Gul Yildiz <zehragul15@gmail.com>
2025-07-02 16:14:01 +0300

    build.gradle.kts settings.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,7 +5,7 @@ allprojects {
     }
 }
 plugins {
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
