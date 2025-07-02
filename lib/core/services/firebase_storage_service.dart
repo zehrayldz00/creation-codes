2504d4c7378cfdd1e6d02f3071376ff64a02fbab
Zehra Gul Yildiz <zehragul15@gmail.com>
2025-07-02 12:14:22 +0300

    firebase storage service cache added.

--- lib/core/services/firebase_storage_service.dart
+++ lib/core/services/firebase_storage_service.dart
@@ -1,9 +1,16 @@
 import 'package:firebase_storage/firebase_storage.dart';
 
 class FirebaseStorageService{
+
+  static final Map<String,String> _cache = {};
+
   static Future<String> getModelDownloadUrl(String modelPath) async {
+    if(_cache.containsKey(modelPath)){
+      return _cache[modelPath]!;
+    }
       final ref = FirebaseStorage.instance.ref().child(modelPath);
       final url = await ref.getDownloadURL();
+      _cache [modelPath] = url;
       return url;
 
   }
